e04f827d8b9fee54faa6f1d6563ba4a39501756f
muxiaonong <1347210890@qq.com>
2022-05-26 20:42:17 +0800

    更新

--- .umirc.ts
+++ .umirc.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   mode: 'site',
   locales: [['zh-CN', '中文']],
   logo: ' ',
+  headScripts: [`localStorage.setItem("dumi:prefers-color", "dark");document.documentElement.setAttribute("data-prefers-color", "dark");`],
   styles: [`.__dumi-default-navbar-tool { display: none !important }; .__dumi-default-navbar-logo{padding-left: 0 !important}`],
   alias: {
     "@": join(__dirname, "src")
